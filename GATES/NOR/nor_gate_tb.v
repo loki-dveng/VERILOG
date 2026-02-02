f24e46079bf230b29084c0b6aef073228e6f2d8b
Lokesh A S <as.lokesh1462004@gmail.com>
2026-02-02 17:35:22 +0530

    Update and rename nor_gate.v to nor_gate_tb.v

---
+++ GATES/NOR/nor_gate_tb.v
@@ -0,0 +1,19 @@
+module nor_gate_tb;
+  reg a,b;
+  wire y;
+  nor_gate g(.a(a), . b(b), .y(y));
+  initial begin
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+  initial begin
+    $monitor("t=%t, a=%0d, b=%0d, y=%0d", $time,a,b,y);
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,nor_gate_tb);
+  end
+  endmodule
