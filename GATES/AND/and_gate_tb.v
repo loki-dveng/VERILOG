f0c14d0cfb7388f49ced07a4edc6894680beda9c
Lokesh A S <as.lokesh1462004@gmail.com>
2026-02-02 16:26:02 +0530

    Create and_gate_tb.v

---
+++ GATES/AND/and_gate_tb.v
@@ -0,0 +1,20 @@
+module and_gate_tb;
+  reg a,b;
+  wire y;
+  and_gate g(.a(a), .b(b), .y(y));
+  initial begin
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+  initial begin
+    $monitor("t=%t, a=%0d, b=%0d, y=%0d", $time,a,b,y);
+  end
+  initial begin
+  $dumpfile("dump.vcd");
+  $dumpvars(0,tb_and_gate);
+  end
+endmodule
+    
