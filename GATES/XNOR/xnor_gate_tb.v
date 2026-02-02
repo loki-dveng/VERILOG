6b016a560d82b61edc5de8f2a080bf8dba12a4ea
LOKESH A S <as.lokesh1462004@gmail.com>
2026-02-02 18:10:47 +0530

    Create xnor_gate_tb.v

---
+++ GATES/XNOR/xnor_gate_tb.v
@@ -0,0 +1,20 @@
+module xnor_gate_tb;
+  reg a,b;
+  wire y;
+  xnor_gate g(.a(a), . b(b), .y(y));
+  initial begin
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+  initial begin
+    $monitor("t=%t, a=%0d, b=%0d, y=%0d", $time,a,b,y);
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,xnor_gate_tb);
+  end
+  endmodule
+    
